c3322e0c40554a3c10cec822e94a03c91aa1dcc9
Andrew Seier <andseier@gmail.com>
2015-11-15 14:40:56 -0500

    Add new `Immutable` class with tests.

---
+++ immutable/immutable.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import, unicode_literals
+
+from collections import OrderedDict
+
+
+class Immutable(object):
+    """
+    Immutable object which adheres to the Mapping and the Sequence protocols.
+
+    * Attributes are kept in `self._ordered_dict`. NEVER MUTATE THIS!
+    * Instantiate with kwargs or args. Instantiating with args preserves order.
+    * Mapping methods get(), items(), keys(), and values() are also included.
+    * Sequence methods index() and count() are also included.
+
+    """
+
+    class ImmutableError(Exception):
+        pass
+
+    def __init__(self, *args, **kwargs):
+        """
+        Instantiate an Immutable instance.
+
+        >>> # tuple instantiation --> Note that this method preserves order!
+        >>> obj = Immutable((('val_0', 0), ('val_1', 1)))
+        >>> # key=value pairs
+        >>> obj = Immutable(val_0=0, val_1=1)
+        >>> # same as above, but by upacking a dict
+        >>> attribute_dict = {'val_0': 0, 'val_1': 1}
+        >>> obj = Immutable(**attribute_dict)
+        >>> # access via '.' or '[]'
+        >>> obj.val_0
+        >>> obj['val_0']
+
+        :param args: (<attr>, <val>,) pairs to add *in order*
+        :param kwargs: Allows you to unpack a dict to create this.
+
+        """
+        reserved_keys = ('get', 'keys', 'values', 'items', 'count', 'index',
+                         '_ordered_dict', '_tuple')
+        ordered_dict = OrderedDict()
+        for key, val in args:
+            if key in kwargs:
+                raise self.ImmutableError('Key in args duplicated in kwargs.')
+            ordered_dict[key] = val
+        ordered_dict.update(kwargs)
+        for key, val in ordered_dict.items():
+            try:
+                hash(key)
+                hash(val)
+            except TypeError:
+                raise self.ImmutableError('Keys and vals must be hashable.')
+            if isinstance(key, int):
+                raise self.ImmutableError('Keys cannot be integers.')
+            if key in reserved_keys:
+                raise self.ImmutableError('Keys cannot be any of these: {}.'
+                                          .format(reserved_keys))
+        self.__dict__['_ordered_dict'] = ordered_dict
+        self.__dict__['_tuple'] = tuple(ordered_dict.values())
+
+    def __contains__(self, item):
+        raise self.ImmutableError('Containment not implemented. Try with '
+                                  'keys(), values(), or items().')
+
+    def __reversed__(self):
+        raise self.ImmutableError('Reversal not implemented. Try with '
+                                  'keys(), values(), or items().')
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.__dict__['_tuple'][key]
+        else:
+            return self.__dict__['_ordered_dict'][key]
+
+    def __setitem__(self, key, value):
+        raise self.ImmutableError('Cannot set items on Immutable.')
+
+    def __getattr__(self, key):
+        if key == 'items':
+            return self.__dict__['_ordered_dict'].items
+        elif key == 'keys':
+            return self.__dict__['_ordered_dict'].keys
+        elif key == 'values':
+            return self.__dict__['_ordered_dict'].values
+        elif key == 'index':
+            return self.__dict__['_tuple'].index
+        elif key == 'count':
+            return self.__dict__['_tuple'].count
+        else:
+            return self.__getitem__(key)
+
+    def __setattr__(self, key, value):
+        raise self.ImmutableError('Cannot set attributes on Immutable.')
+
+    def __cmp__(self, other):
+        raise self.ImmutableError('Only equality comparisons implemented.')
+
+    def __eq__(self, other):
+        if not isinstance(other, Immutable):
+            return False
+        return hash(self) == hash(other)
+
+    def __ne__(self, other):
+        if not isinstance(other, Immutable):
+            return True
+        return hash(self) != hash(other)
+
+    def __len__(self):
+        return len(self.__dict__['_tuple'])
+
+    def __iter__(self):
+        raise self.ImmutableError('Iteration not implemented. Try with '
+                                  'keys(), values(), or items().')
+
+    def __hash__(self):
+        return hash(tuple(self._ordered_dict.items()))
+
+    def __str__(self):
+        return '{}'.format(self.__repr__())
+
+    def __unicode__(self):
+        return u'{}'.format(self.__repr__())
+
+    def __repr__(self):
+        keys_repr = u', '.join(u'{}={}'.format(key, repr(val))
+                               for key, val in self.items())
+        return u'Immutable({})'.format(keys_repr)
+
+    def __dir__(self):
+        return list(self.keys())
--- immutable/tests.py
+++ immutable/tests.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from unittest import TestCase
 
-from immutable import ImmutableFactory
+from immutable import Immutable, ImmutableFactory
 
 
 class TestImmutableObjectFactory(TestCase):
@@ -119,3 +119,169 @@ class TestImmutableObjectFactory(TestCase):
         with self.assertRaisesRegexp(TypeError,
                                      "does not support item assignment"):
             obj[0] = 'to this'
+
+
+class TestImmutable(TestCase):
+
+    def setUp(self):
+        self.unordered_immutable = Immutable(black='black', white='white',
+                                             red='red', blue='blue')
+        self.ordered_immutable = Immutable(
+            ('zero', 0), ('one', 1), ('two', 2), ('three', 3)
+        )
+
+    def test_create_empty(self):
+
+        # should be able to instantiate without args.
+
+        obj = Immutable()
+        self.assertEqual(obj.__class__.__name__, 'Immutable')
+
+    def test_instantiation(self):
+
+        # you can create via a tuple, an unpacked dict, or both.
+
+        tup_instantiation = Immutable(('blah', 10))
+        self.assertEqual(tup_instantiation.blah, 10)
+
+        dict_instantiation = Immutable(**{'okie': 'dokie'})
+        self.assertEqual(dict_instantiation.okie, 'dokie')
+
+        both_instantiation = Immutable(('blah', 10), **{'okie': 'dokie'})
+        self.assertEqual(both_instantiation.items(),
+                         [('blah', 10), ('okie', 'dokie')])
+
+    def test_keys(self):
+
+        # the keys() api should be preserved
+
+        self.assertEqual(set(self.unordered_immutable.keys()),
+                         {'black', 'white', 'red', 'blue'})
+
+        # if given as a tuple, they should keep the given order
+        self.assertEqual(self.ordered_immutable.keys(),
+                         ['zero', 'one', 'two', 'three'])
+
+    def test_values(self):
+
+        # the values() api should be preserved
+
+        self.assertEqual(set(self.unordered_immutable.values()),
+                         {'black', 'white', 'red', 'blue'})
+
+        # if given as a tuple, they should keep the given order
+        self.assertEqual(self.ordered_immutable.values(), [0, 1, 2, 3])
+
+    def test_items(self):
+
+        # the items() api should be preserved
+
+        self.assertEqual(set(self.unordered_immutable.items()),
+                         {('black', 'black'), ('white', 'white'),
+                          ('red', 'red'), ('blue', 'blue')})
+
+        # if given as a tuple, they should keep the given order
+        self.assertEqual(self.ordered_immutable.items(),
+                         [('zero', 0), ('one', 1), ('two', 2), ('three', 3)])
+
+    def test_index(self):
+
+        # the index() api should be preserved (it acts on the values).
+
+        self.assertEqual(self.ordered_immutable.index(0), 0)
+        self.assertEqual(self.ordered_immutable.index(1), 1)
+
+    def test_count(self):
+
+        # the count() api should be preserved (it acts on the values).
+
+        obj = Immutable(**{k: 'blah' for k in 'abcdefg'})
+        self.assertEqual(obj.count('blah'), 7)
+        self.assertEqual(obj.count('nope'), 0)
+
+    def test_containment(self):
+
+        # we don't support containment because it's ambiguous
+
+        regex = 'Containment not implemented.'
+        with self.assertRaisesRegexp(Immutable.ImmutableError, regex):
+            'white' in self.unordered_immutable
+
+    def test_reversal(self):
+
+        # we don't support reversal because it's ambiguous
+
+        regex = 'Reversal not implemented.'
+        with self.assertRaisesRegexp(Immutable.ImmutableError, regex):
+            reversed(self.unordered_immutable)
+
+    def test_equality(self):
+
+        # equality works by hashing two immutable instances
+
+        self.assertFalse(self.ordered_immutable == self.unordered_immutable)
+
+        args = [('a', 1), ('b', 2)]
+        imm_0 = Immutable(*args)
+        imm_1 = Immutable(*args)
+        self.assertFalse(imm_0 == args)
+        self.assertTrue(imm_0 == imm_1)
+
+
+    def test_non_equality_comparisons(self):
+
+        # equality works by hashing two immutable instances
+
+        self.assertTrue(self.ordered_immutable != self.unordered_immutable)
+
+        args = [('a', 1), ('b', 2)]
+        imm_0 = Immutable(*args)
+        imm_1 = Immutable(*args)
+        self.assertTrue(imm_0 != args)
+        self.assertFalse(imm_0 != imm_1)
+
+    def test_attribute_access(self):
+
+        # You should be able to access via both getitem or getattr with indices
+        # or keys.
+
+        self.assertEqual(self.ordered_immutable.zero, 0)
+        self.assertEqual(self.ordered_immutable['zero'], 0)
+        self.assertEqual(self.ordered_immutable[0], 0)
+        self.assertEqual(self.ordered_immutable[-4], 0)
+
+    def test_change_attribute(self):
+
+        # you can't!
+
+        # should not be able to change with key
+        with self.assertRaisesRegexp(Immutable.ImmutableError,
+                                     'Cannot set items on Immutable.'):
+            self.ordered_immutable['zero'] = 10
+
+        # should not be able to change with index
+        with self.assertRaisesRegexp(Immutable.ImmutableError,
+                                     'Cannot set items on Immutable.'):
+            self.ordered_immutable[0] = 10
+
+        # should not be able to change with negative index
+        with self.assertRaisesRegexp(Immutable.ImmutableError,
+                                     'Cannot set items on Immutable.'):
+            self.ordered_immutable[-4] = 10
+
+        # should not be able to change with '.' access
+        with self.assertRaisesRegexp(Immutable.ImmutableError,
+                                     'Cannot set attributes on Immutable.'):
+            self.ordered_immutable.zero = 10
+
+        # finally, show how we *can* change it! NEVER DO THIS!
+        self.ordered_immutable._ordered_dict['zero'] = 10
+        self.assertEqual(self.ordered_immutable['zero'], 10)
+
+    def test___dir__(self):
+
+        # __dir__ should return a list of the keys.
+
+        dir_list = self.ordered_immutable.__dir__()
+        expected_dir_list = ['zero', 'one', 'two', 'three']
+        self.assertEqual(dir_list, expected_dir_list)
